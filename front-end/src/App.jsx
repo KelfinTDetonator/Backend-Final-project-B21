a3909fa78b8063219b420f9ca9fb4dba2077bd04
Muhammad Putra Wibisono <120109799+MPutraWibisono@users.noreply.github.com>
2023-11-30 21:18:43 +0800

    Frontend (#4)
    * Homepage (#2)
    
    * feat:homepage 1/2
    
    * homepage:3/4
    
    * Homepage (#3)
    
    * feat:homepage 1/2
    
    * homepage:3/4
    
    * homepage

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
+import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 
 function App() {
