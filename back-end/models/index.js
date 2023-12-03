45816cece0253a772a7c7979d8213dbdc9986860
KelfinTDetonator <kelfinantoni@gmail.com>
2023-12-04 01:30:01 +0700

    add: all models from prisma

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -3,6 +3,11 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = {
+  user: prisma.user,
+  profile: prisma.profile,
   category: prisma.category,
+  course: prisma.course,
+  order: prisma.order,
+  chapter: prisma.chapter,
   material: prisma.material,
 };
