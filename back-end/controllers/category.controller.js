9b8149aa8d443b882f3dda42ffb8dc1714cb68c9
KelfinTDetonator <kelfinantoni@gmail.com>
2023-12-04 14:59:07 +0700

    chore: rewrite typo; add controller error handler

--- back-end/controllers/category.controller.js
+++ back-end/controllers/category.controller.js
@@ -26,7 +26,7 @@ module.exports = {
 
   getCategory: async (req, res) => {
     try {
-      const id = parseInt(req.params.id);
+      const id = Number(req.params.id);
       if (!id) { return res.status(400).json({ error: true, message: "Bad Request Error" }); }
 
       const categoryData = await category.findUnique({ where: { id } });
@@ -59,39 +59,49 @@ module.exports = {
   },
 
   patchCategory: async (req, res) => {
-    const { name } = req.body;
-    const id = parseInt(req.params.id);
+    try {
+      const { name } = req.body;
+      const id = Number(req.params.id);
 
-    if (!(id && name)) { return res.status(400).json({ error: true, message: "Bad Request Error" }); }
+      if (!(id && name)) { return res.status(400).json({ error: true, message: "Bad Request Error" }); }
 
-    const categoryData = await category.findUnique({ where: { id } });
-    if (!categoryData) { return res.status(404).json({ error: true, message: "Category Not Found" }); }
+      const categoryData = await category.findUnique({ where: { id } });
+      if (!categoryData) { return res.status(404).json({ error: true, message: "Category Not Found" }); }
 
-    await category.update({
-      where: {
-        id,
-      },
-      data: {
-        name,
-      },
-    });
+      await category.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+        },
+      });
 
-    return res.status(200).json({
-      error: false,
-      message: "200 OK",
-      category: categoryData,
-    });
+      return res.status(200).json({
+        error: false,
+        message: "200 OK",
+        category: categoryData,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
   },
 
   deleteCategory: async (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!id) { return res.status(400).json({ error: true, message: "Bad Request Error" }); }
+    try {
+      const id = Number(req.params.id);
+      if (!id) { return res.status(400).json({ error: true, message: "Bad Request Error" }); }
 
-    const categoryData = await category.findUnique({ where: { id } });
-    if (!categoryData) { return res.status(404).json({ error: true, message: "Category Not Found" }); }
+      const categoryData = await category.findUnique({ where: { id } });
+      if (!categoryData) { return res.status(404).json({ error: true, message: "Category Not Found" }); }
 
-    await category.delete({ where: { id } });
+      await category.delete({ where: { id } });
 
-    return res.status(204).json({ error: false, message: "Resource deleted successfully" });
+      return res.status(200).json({ error: false, message: "Resource deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
   },
 };
