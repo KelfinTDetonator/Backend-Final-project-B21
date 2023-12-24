b51d60901b436dc180ed99b433ed5e088607d6c9
KelfinTDetonator <kelfinantoni@gmail.com>
2023-12-24 21:49:00 +0700

    feat: login admin

--- back-end/routers/user.router.js
+++ back-end/routers/user.router.js
@@ -6,7 +6,8 @@ const checkRole = require("../middleware/checkRole");
 const checkToken = require("../middleware/checkToken");
 
 router.delete("/:id", controller.deleteUser);
-router.get("/", controller.getAllUsers);
+// router.get("/", controller.getAllUsers);
+router.get("/", checkToken, checkRole(["admin", "user"]), controller.getAllUsers);
 router.get("/:id", controller.getById);
 router.post("/otp", controller.otp);
 router.post("/verify", controller.verify);
@@ -15,4 +16,6 @@ router.post("/login", controller.login);
 router.post("/forget-password", controller.forgetPassword);
 router.post("/insert-password", controller.insertPassword);
 router.post("/update-password", checkToken, checkRole(["admin", "user"]), controller.updatePassword);
+router.post("/admin", controller.loginAdmin);
+
 module.exports = router;
