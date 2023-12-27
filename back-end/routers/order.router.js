52c4f4283e4ddadb64c8141ab98c12231453d129
Kelfin Antonius <kelfinantoni@gmail.com>
2023-12-27 11:04:35 +0000

    chore: fix typo

--- back-end/routers/order.router.js
+++ back-end/routers/order.router.js
@@ -6,7 +6,7 @@ const checkRole = require("../middleware/checkRole");
 // todo: checkRole(["user"]),
 router.post("/create", checkToken, checkRole(["user", "admin"]), orderController.createNewOrder);
 router.patch("/update", checkToken, checkRole(["admin", "user"]), orderController.patchOrder);
-router.delete("/delete/:id", checkToken, checkRole(["admin"]), orderController.deleteCourseFromOrder);
+router.delete("/delete/:id", checkToken, checkRole(["admin"]), orderController.deleteOrderData);
 // router.post("/midtrans/notif", orderController.handlePaymentNotification);
 
 module.exports = router;
